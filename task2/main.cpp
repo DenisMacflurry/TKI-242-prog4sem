0b8607330d5967113d42d4567b22a14aab710750
Denis McFlurry <bdjibm@bk.ru>
2026-05-29 18:59:35 +0300

    add input function

--- task2/main.cpp
+++ task2/main.cpp
@@ -6,15 +6,52 @@
 
 using namespace std;
 
-int main() {
-    vector<int> V = {10, 20, 30, 40, 50, 60, 70};
-    list<int> L = {1, 2, 3, 4, 5, 6, 7, 8};
+template<typename Container>
+void inputContainer(Container& container, const string& prompt) {
+    cout << prompt << endl;
+    copy(
+        istream_iterator<int>(cin),
+        istream_iterator<int>(),
+        back_inserter(container)
+    );
+    cin.clear();
+    cin.ignore();
+}
 
-    auto list_it = next(L.begin(), 5);
-    L.insert(list_it, V.rend() - 5, V.rend());
+template<typename Container>
+bool checkEmpty(const Container& container, const string& containerName) {
+    if (container.empty()) {
+        cout << "Ошибка: " << containerName << " пуст!" << endl;
+        return true;
+    }
+    return false;
+}
 
-    cout << "Итоговый список: ";
+int main(void) {
+    vector<int> V;
+    list<int> L;
+
+    inputContainer(V, "Введите элементы вектора:");
+
+    inputContainer(L, "Введите элементы списка:");
+
+    if (checkEmpty(V, "Вектор")) {
+        return 1;
+    }
+
+    if (checkEmpty(L, "Список")) {
+        return 1;
+    }
+
+    auto list_it = next(L.begin(), min(5, (int)L.size()));
+    L.insert(list_it, V.rend() - min(5, (int)V.size()), V.rend());
+
+    cout << "\nВектор: ";
+    for (int x : V) cout << x << " ";
+    
+    cout << "\nИтоговый список: ";
     for (int x : L) cout << x << " ";
+    cout << endl;
     
     return 0;
 }
